dad612df1897adb4c024a2938a49bc69e1db6620
KyleCLim <kyleclarenzlim@gmail.com>
2024-07-29 23:36:52 +0800

    Update security

--- frontend/src/Config/firebaseConfig.js
+++ frontend/src/Config/firebaseConfig.js
@@ -1,11 +1,4 @@
 const firebaseConfig = {
-    // apiKey: "AIzaSyCdhGqwAm8cBqDMwS2rYbsMaV4TeDr6KaU",
-    // authDomain: "crypto-tracker-46561.firebaseapp.com",
-    // projectId: "crypto-tracker-46561",
-    // storageBucket: "crypto-tracker-46561.appspot.com",
-    // messagingSenderId: "333107388615",
-    // appId: "1:333107388615:web:dfacfea8563067a4f85ee2",
-
     apiKey: process.env.REACT_APP_FB_APIKEY,
     authDomain: process.env.REACT_APP_FB_AUTHDOMAIN,
     projectId: process.env.REACT_APP_FB_PROJECTID,
